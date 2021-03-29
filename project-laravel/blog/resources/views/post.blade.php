0abc3941074643175e7a641f2c9dc25fc3c3c28c
andreistoleriu5ps <andrei.stoleriu@5psolutions.eu>
2021-03-29 13:24:28 +0300

    laravel setup

---
+++ project-laravel/blog/resources/views/post.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<title>My First Laravel BLog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+      <?p $post;?>
+    </article>
+    <a href="/">Go Back</a>
+</body>
